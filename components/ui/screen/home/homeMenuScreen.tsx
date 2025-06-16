6b1049497101638b0d5b56ef7ba83dc40e6a01b9
anjusena-wickramasinghe <anjalasenani@gmail.com>
2025-06-16 12:54:14 +0530

    Add files via upload

---
+++ components/ui/screen/home/homeMenuScreen.tsx
@@ -0,0 +1,28 @@
+import {View,Text,StyleSheet} from 'react-native';
+import {Menu} from 'react-native-paper';
+import { COLORS } from '@/constants/colorpallets';
+
+
+export default function homeMenuScreen(){
+    return(
+        <View style={styles.container}>
+           <Menu.Item leadingIcon="redo" onPress={() => {}} title="My order" />
+    <Menu.Item leadingIcon="undo" onPress={() => {}} title="settings" />
+    <Menu.Item leadingIcon="content-cut" onPress={() => {}} title="Vouchers"  />
+    <Menu.Item leadingIcon="content-copy" onPress={() => {}} title="Daily Deals"  />
+    <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="Nortification" />
+        <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="Latest Products" />
+            <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="Profile" />
+                <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="Customer care" />
+                    <Menu.Item leadingIcon="content-paste" onPress={() => {}} title="logout" />
+        </View>
+    )
+
+}
+
+const styles= StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: COLORS.red
+    }
+})
