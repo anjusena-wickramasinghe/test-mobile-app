b0d52335fd4f00755171d3fff478ff7fb3d61861
anjusena-wickramasinghe <anjalasenani@gmail.com>
2025-06-18 21:00:54 +0530

    Add files via upload

---
+++ components/route/RoleRoute.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const RoleController=require('../controller/RoleController');
+
+router.post('/create-role', RoleController.createRole)
+
+module.exports=router;
